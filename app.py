38b3358d79a27fb1e21bb66eda4dce6d1679ef15
Devsixth <129255859+Devsixth@users.noreply.github.com>
2024-04-21 15:35:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,21 @@ import pandas as pd
 
 def load_data(file_path):
     try:
-        data = pd.read_csv(file_path)
+        df = pd.read_csv(file_path)
     except FileNotFoundError:
-        data = pd.DataFrame(columns=['Name', 'MobNo', 'BorNB', 'Age', 'Gender', 'TS1', 'TS2', 'TS3', 'TS4', 'TS5', 'BTS6', 'NBTS6'])
-    return data
+        st.error("File not found!")
+        st.stop()  # Stop execution if file not found
+    return df
 
-def save_data(df, file_path):
-    df.to_csv(file_path, index=False)
-
-def display_user_details(user_id, data):
-    user_details = data[data['Name'] == user_id] if user_id in data['Name'].values else data[data['MobNo'] == user_id]
-    user_details = user_details[['Name', 'MobNo', 'BorNB', "Age", "Gender"]]
-    st.write(user_details)
 def main():
-    st.title('Gen Population Testing Battery')
+    st.title('Select Name from CSV')
 
-    file_path = 'C:/Users/Admin/Desktop/gen/SimData - SimData.csv'
+    file_path = 'C:/Users/Admin/Desktop/gen/SimData - SimData.csv'  # Update with your CSV file path
     data = load_data(file_path)
 
+    selected_name = st.selectbox('Select Name:', data['Name'].unique())
 
-    # Select search option
-    select_option = st.sidebar.selectbox('Select search option:', ['Name', 'Phone Number'])
-
-    if select_option == 'Name':
-        names = data['Name'].unique()
-        selected_name = st.sidebar.selectbox('Select name:', options=names, default=names, key="n")
-        display_user_details(selected_name, data)
-    else:
-        phone_numbers = data['MobNo'].unique()
-        selected_phone = st.sidebar.selectbox('Select phone number:', phone_numbers)
-        display_user_details(selected_phone, data)
+    st.write("You selected:", selected_name)
 
 if __name__ == "__main__":
     main()
