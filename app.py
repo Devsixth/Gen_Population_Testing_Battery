88550e965360db700ea6262ee92e73bd4ece8495
Devsixth <129255859+Devsixth@users.noreply.github.com>
2024-04-21 12:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
-
+@st.cache(allow_output_mutation=True)
 def load_data(file_path):
     try:
-        df = pd.read_csv(file_path)
+        data = pd.read_csv(file_path)
     except FileNotFoundError:
-        df = pd.DataFrame(columns=['Name', 'MobNo', 'BorNB', 'Age', 'Gender', 'TS1', 'TS2', 'TS3', 'TS4', 'TS5', 'BTS6', 'NBTS6'])
-    return df
+        data = pd.DataFrame(columns=['Name', 'MobNo', 'BorNB', 'Age', 'Gender', 'TS1', 'TS2', 'TS3', 'TS4', 'TS5', 'BTS6', 'NBTS6'])
+    return data
 
 def save_data(df, file_path):
     df.to_csv(file_path, index=False)
@@ -15,24 +15,22 @@ def display_user_details(user_id, data):
     user_details = data[data['Name'] == user_id] if user_id in data['Name'].values else data[data['MobNo'] == user_id]
     user_details = user_details[['Name', 'MobNo', 'BorNB', "Age", "Gender"]]
     st.write(user_details)
-
 def main():
     st.title('Gen Population Testing Battery')
 
     file_path = 'C:/Users/Admin/Desktop/gen/SimData - SimData.csv'
     data = load_data(file_path)
 
-    # Precompute unique names and phone numbers
-    names = data['Name'].unique()
-    phone_numbers = data['MobNo'].unique()
 
     # Select search option
-    select_option = st.sidebar.radio('Select search option:', ['Name', 'Phone Number'])
+    select_option = st.sidebar.selectbox('Select search option:', ['Name', 'Phone Number'])
 
     if select_option == 'Name':
+        names = data['Name'].unique()
         selected_name = st.sidebar.selectbox('Select name:', names)
         display_user_details(selected_name, data)
     else:
+        phone_numbers = data['MobNo'].unique()
         selected_phone = st.sidebar.selectbox('Select phone number:', phone_numbers)
         display_user_details(selected_phone, data)
 
